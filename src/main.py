1562f70119ce5090d2df1d7f2718c8cc9f6247c8
Robi Haider <robihaiderme@gmail.com>
2026-02-27 15:24:56 +0600

    Add basic error handling to the calculator for divide

--- src/main.py
+++ src/main.py
@@ -8,5 +8,5 @@ from utils import add, divide, multiply, subtract
 print(add(5, 3))
 print(subtract(5, 3))
 print(multiply(5, 3))
-print(divide(5, 3))
+print(divide(5, 3)) # Testing normal division case
 print(divide(5, 0)) # Testing division by zero case
