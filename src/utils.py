0c3ddad718f118053d788af41cde1e88d5b61c36
Robi Haider <robihaiderme@gmail.com>
2026-02-27 14:31:05 +0600

    Add multiply function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
+# Created addition functions in utils.py
 def add(a, b):
     return a + b
+# Created subtraction functions in utils.py
 
 def subtract(a, b):
     return a - b
+
+# Created Multiply functions in utils.py
+
+def multiply(a, b):
+    return a * b
+
