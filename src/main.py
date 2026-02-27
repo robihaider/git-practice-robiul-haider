4bd67ab4b57c83d638bbee17c026fdde1f3b2d9f
Robi Haider <robihaiderme@gmail.com>
2026-02-27 14:57:57 +0600

    Finally Add Basic Calculator

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ from datetime import date
 print("K M Robiul Haider")
 print(date.today())
 
-from utils import add, subtract
+from utils import add, divide, multiply, subtract
 
 print(add(5, 3))
 print(subtract(5, 3))
+print(multiply(5, 3))
+print(divide(5, 3))
+print(divide(5, 0))
